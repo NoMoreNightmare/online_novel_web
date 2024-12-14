13cb2703dbc674687485bc6b9c79785999794b06
s2517285 <s2517285@ed.ac.uk>
2024-12-14 21:53:53 +0800

    feat: 新增MyFileController
    用于执行文件相关的操作的controller

--- novel-front/src/main/java/com/java2nb/novel/controller/MyFileController.java
+++ novel-front/src/main/java/com/java2nb/novel/controller/MyFileController.java
@@ -1,21 +1,21 @@
-//package com.java2nb.novel.controller;
-//
-//import lombok.SneakyThrows;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//
-//@Controller
-//@RequestMapping("file")
-//@Slf4j
-//public class MyFileController {
-//    @GetMapping("getVerify")
-//    @SneakyThrows
-//    public String verify(HttpServletRequest request, HttpServletResponse response) {
-//        request.getParameter()
-//    }
-//}
+package com.java2nb.novel.controller;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+@RequestMapping("file")
+@Slf4j
+public class MyFileController {
+    @GetMapping("getVerify")
+    @SneakyThrows
+    public String verify(HttpServletRequest request, HttpServletResponse response) {
+        request.getParameter()
+    }
+}
