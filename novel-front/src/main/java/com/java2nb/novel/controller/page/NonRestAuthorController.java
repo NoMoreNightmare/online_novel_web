f8995fea2e651d429d0ed6a5f57440ac06133271
s2517285 <s2517285@ed.ac.uk>
2024-12-26 20:51:32 +0800

    fix: 修复了作家注册时无反应的bug
    新增了checkPenName接口，修复了注册作家时提交表单无反应的问题

--- novel-front/src/main/java/com/java2nb/novel/controller/page/NonRestAuthorController.java
+++ novel-front/src/main/java/com/java2nb/novel/controller/page/NonRestAuthorController.java
@@ -6,12 +6,9 @@ import com.java2nb.novel.core.utils.JwtTokenUtil;
 import com.java2nb.novel.entity.Author;
 import com.java2nb.novel.service.MyAuthorService;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
